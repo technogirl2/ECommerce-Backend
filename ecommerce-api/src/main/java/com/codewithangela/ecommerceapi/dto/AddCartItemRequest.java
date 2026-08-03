a0f780e3c01f27a85db4905096e5abba3b7a5bfb
Angela <angelaliang1234@gmail.com>
2026-08-03 13:28:38 -0400

    adding backend implementation of cart items

---
+++ ecommerce-api/src/main/java/com/codewithangela/ecommerceapi/dto/AddCartItemRequest.java
@@ -0,0 +1,3 @@
+package com.codewithangela.ecommerceapi.dto;
+
+public record AddCartItemRequest(int productId, int quantity) {}
---
+++ ecommerce-api/src/main/java/com/codewithangela/ecommerceapi/service/CartService.java
@@ -0,0 +1,90 @@
+package com.codewithangela.ecommerceapi.service;
+
+import com.codewithangela.ecommerceapi.dao.CartItemRepo;
+import com.codewithangela.ecommerceapi.dao.CartRepo;
+import com.codewithangela.ecommerceapi.dao.ProductRepo;
+import com.codewithangela.ecommerceapi.model.Cart;
+import com.codewithangela.ecommerceapi.model.CartItem;
+import com.codewithangela.ecommerceapi.model.Product;
+import com.codewithangela.ecommerceapi.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CartService {
+
+    @Autowired
+    private CartRepo cartRepo;
+
+    @Autowired
+    private CartItemRepo cartItemRepo;
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    public Cart getCartForUser(User user) {
+        return cartRepo.findByUserId(user.getId())
+                .orElseGet(() -> createCartForUser(user));
+    }
+
+    private Cart createCartForUser(User user) {
+        Cart cart = new Cart();
+        cart.setUser(user);
+        return cartRepo.save(cart);
+    }
+
+    public Optional<Cart> addItem(User user, int productId, int quantity) {
+        Optional<Product> product = productRepo.findById(productId);
+        if (product.isEmpty()) {
+            return Optional.empty();
+        }
+
+        Cart cart = getCartForUser(user);
+        CartItem item = new CartItem();
+        item.setCart(cart);
+        item.setProduct(product.get());
+        item.setQuantity(quantity);
+        cart.getItems().add(cartItemRepo.save(item));
+
+        return Optional.of(cart);
+    }
+
+    public Optional<Cart> updateItemQuantity(User user, int itemId, int quantity) {
+        Cart cart = getCartForUser(user);
+        Optional<CartItem> existingItem = findItemInCart(cart, itemId);
+        if (existingItem.isEmpty()) {
+            return Optional.empty();
+        }
+
+        CartItem item = existingItem.get();
+        if (quantity <= 0) {
+            cart.getItems().remove(item);
+            cartItemRepo.delete(item);
+        } else {
+            item.setQuantity(quantity);
+            cartItemRepo.save(item);
+        }
+
+        return Optional.of(cart);
+    }
+
+    public Optional<Cart> removeItem(User user, int itemId) {
+        Cart cart = getCartForUser(user);
+        Optional<CartItem> existingItem = findItemInCart(cart, itemId);
+        if (existingItem.isEmpty()) {
+            return Optional.empty();
+        }
+
+        cart.getItems().remove(existingItem.get());
+        cartItemRepo.delete(existingItem.get());
+        return Optional.of(cart);
+    }
+
+    private Optional<CartItem> findItemInCart(Cart cart, int itemId) {
+        return cart.getItems().stream()
+                .filter(item -> item.getId() == itemId)
+                .findFirst();
+    }
+}
